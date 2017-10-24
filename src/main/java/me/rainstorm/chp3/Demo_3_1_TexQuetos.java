0a1ef221eb8c82462baf7f73cae3d89b88c659b5
Rainstorm <rainstorm.me@outlook.com>
2017-10-24 18:01:46 +0800

    chpter3 update
    Demo:
    - 3-1 Tex Quetos
    - 3-2 WERTYU

---
+++ src/main/java/me/rainstorm/chp3/Demo_3_1_TexQuetos.java
@@ -0,0 +1,61 @@
+package me.rainstorm.chp3;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *  题目描述：
+ *      在 Tex 中，左双引号是"``",右双引号是"""。
+ *      输入一篇包含双引号的文章，你的任务是把它转换成 Tex 的格式。
+ */
+public class Demo_3_1_TexQuetos {
+
+    @Test
+    public void test1(){
+        basic(new String("\"To be or not to be,\" quoth the Bard, \"that is the question\"."));
+    }
+
+    @Test
+    public void test2(){
+        update();
+    }
+
+    public void basic(String str){
+        boolean isLeft = true;
+        for(int i = 0; i < str.length(); ++i){
+            if(str.charAt(i) == '"'){
+                if(isLeft){
+                    System.out.print("``");
+                }else {
+                    System.out.print("''");
+                }
+                isLeft = !isLeft;
+                continue;
+            }
+            System.out.print(str.charAt(i));
+        }
+        System.out.println();
+    }
+
+    public void update(){
+        try(BufferedReader cin = new BufferedReader(new InputStreamReader(System.in))) {
+            int ch;
+            boolean isLeft = true;
+            while (cin.ready()){
+                if((ch = cin.read()) != -1){
+                    if(ch == '"'){
+                        System.out.printf("%s", isLeft ? "``" : "''");
+                        isLeft = !isLeft;
+                    }else {
+                        System.out.printf("%c", ch);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/me/rainstorm/chp3/Demo_3_2_Wertyu.java
@@ -0,0 +1,44 @@
+package me.rainstorm.chp3;
+
+import org.junit.Test;
+
+/**
+ * 题目描述：
+ *     把手放在键盘上时，稍不注意就会往右错一位。
+ *     这样，输入 Q 会变成输入 W，输入 J 会变成输入 K 等。
+ *
+ *     输入一个错位后敲出的字符串（所有字母均为大写），
+ *     输出打字员本来想打出的句子。输入保证合法，
+ *     即一定是错位之后的字符串。
+ *     例如输入中不会出现大写字母 A。
+ */
+public class Demo_3_2_Wertyu {
+
+    private static final char[] CONST_TABLE = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./".toCharArray();
+
+    @Test
+    public void test(){
+        basic("O S, GOMR YPFSU/".toCharArray());
+    }
+
+    /**
+     *  时间复杂度：O(n)
+     *  空间复杂度：O(1)
+     *
+     */
+    public void basic(char[] str) {
+        for(int i = 0, j; i < str.length; ++i){
+            for(j = 0; j < CONST_TABLE.length; ++j){
+                if(CONST_TABLE[j] == str[i]){
+                    System.out.print(CONST_TABLE[j-1]);
+                    break;
+                }
+            }
+            if(j == CONST_TABLE.length){
+                System.out.print(str[i]);
+            }
+        }
+        System.out.println();
+    }
+
+}
