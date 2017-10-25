68b626579e38fcd9dba773e7ade61d31bfd1c369
Rainstorm <rainstorm.me@outlook.com>
2017-10-25 11:57:49 +0800

    chapter3 update
    - 3-3 Palindromes
    - 3-4 Master-Mind Hints

---
+++ src/main/java/me/rainstorm/chp3/Demo_3_3_Palindromes.java
@@ -0,0 +1,71 @@
+package me.rainstorm.chp3;
+
+import org.junit.Test;
+
+
+/**
+ * 问题描述
+ * 输入一个字符串，判断它是否为回文串以及镜像串。输入字符串保证不含数字 0。
+ * 所谓回文串，就是反转以后和原串相同，如 abba 和 madam。所有镜像串，就是左右镜像之后和原串相同，
+ * 如 2S 和 3AIAE。注意，并不是每个字符在镜像之后都能得到一个合法字符。在本题中，每个字符的镜像如下：
+ * <p>
+ * A-Z1-9
+ * "A   3  HIL JM O   2TUVWXY51SE Z  8 "
+ */
+public class Demo_3_3_Palindromes {
+
+    private static final char[] MIRROR = "A   3  HIL JM O   2TUVWXY51SE Z  8 ".toCharArray();
+    private static String[] msgs = new String[]{
+            "is not a palindrome.",
+            "is a regular palindrome.",
+            "is a mirrored string.",
+            "is a mirrored palindrome."
+    };
+
+    @Test
+    public void test1() {
+        basic("NOTAPALINDROME");
+    }
+
+    @Test
+    public void test2() {
+        basic("ISAPALINILAPASI");
+    }
+
+    @Test
+    public void test3() {
+        basic("2A3MEAS");
+    }
+
+    @Test
+    public void test4() {
+        basic("ATOYOTA");
+    }
+
+    /**
+     * 时间复杂度： O(n)
+     * 空间复杂度: O(n) // toCharArray() 方法 new 了一个新的数组并拷贝源数据到新数组
+     */
+    public void basic(String str) {
+        char[] chars = str.toCharArray();
+        int isPalindrome = 1;
+        int isMirror = 1;
+        for (int i = 0, len = chars.length >> 1; (isPalindrome == 1 || isMirror == 1) && i < len; ++i) {
+            if (chars[i] != chars[chars.length - i - 1]) {
+                isPalindrome = 0;
+            }
+            if (chars[i] != getMirror(chars[chars.length - i - 1])) {
+                isMirror = 0;
+            }
+        }
+        System.out.printf("%s -- %s\n", str, msgs[2 * isMirror + isPalindrome]);
+    }
+
+    private char getMirror(char origin) {
+        if (Character.isDigit(origin)) {
+            return MIRROR[origin - '0' + 25];
+        }
+        return MIRROR[origin - 'A'];
+    }
+
+}
---
+++ src/main/java/me/rainstorm/chp3/Demo_3_4_MasterMindHints.java
@@ -0,0 +1,82 @@
+package me.rainstorm.chp3;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 问题描述：
+ * 实现一个井段的猜数字游戏。给定答案的序列和用户猜的序列，
+ * 统计有多少数字位正确，有多少数字在两个序列都出现过但位置不对
+ */
+public class Demo_3_4_MasterMindHints {
+
+    private static int col;
+    private static int row;
+    private static int nums[][];
+
+    @Test
+    public void test() {
+        col = 4;
+        row = 6;
+        nums = new int[][]{
+                {1, 3, 5, 5},
+                {1, 1, 2, 3},
+                {4, 3, 3, 5},
+                {6, 5, 5, 1},
+                {6, 1, 3, 5},
+                {1, 3, 5, 5}
+        };
+        basic();
+        System.out.println();
+    }
+
+    @Test
+    public void test1() {
+        col = 10;
+        row = 5;
+        nums = new int[][]{
+                {1, 2, 2, 2, 4, 5, 6, 6, 6, 9},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 1},
+                {1, 1, 2, 2, 3, 3, 4, 4, 5, 5},
+                {1, 2, 1, 3, 1, 5, 1, 6, 1, 9},
+                {1, 2, 2, 5, 5, 5, 6, 6, 6, 7}
+        };
+        basic();
+        System.out.println();
+    }
+
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     *
+     */
+    private void basic() {
+        int[] answer = new int[10];
+        int[] guess = new int[10];
+        for (int i = 1; i < row; ++i) {
+
+            // 每计算一行进行初始化参数。
+            int match = 0;
+            int notMatch = 0;
+            Arrays.fill(answer, 0);
+            Arrays.fill(guess, 0);
+
+            for(int j = 0; j < col; ++j){
+                if(nums[0][j] == nums[i][j]){
+                    match++;
+                }else {
+                    answer[nums[0][j]]++;
+                    guess[nums[i][j]]++;
+                }
+            }
+            for(int j = 0; j < 10; ++j){
+                if(answer[j] > 0 && guess[j] > 0){
+                    notMatch++;
+                }
+            }
+            System.out.println(match + ", " + notMatch);
+        }
+    }
+
+}
