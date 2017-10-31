5a9beac5bc6c9c814ca078f8ddc34cb94302dc2b
Rainstorm <rainstorm.me@outlook.com>
2017-10-31 18:58:18 +0800

    chapter 3 & 4 update
    - 3-5 Puzzle [Accepted]
    - 4-1 Ancient Cipher [Accepted]

---
+++ src/main/java/me/rainstorm/chp3/Pra_3_5_Puzzle.java
@@ -0,0 +1,187 @@
+package me.rainstorm.chp3;
+
+import org.junit.Test;
+
+import java.util.Scanner;
+
+/**
+ * 题目描述：
+ *      https://uva.onlinejudge.org/external/2/227.pdf
+ */
+public class Pra_3_5_Puzzle {
+    private static final int SIZE = 5;
+    private static char[][] square = new char[SIZE][SIZE];
+
+    @Test
+    public void test() {
+        square[0] = "TRGSJ".toCharArray();
+        square[1] = "XDOKI".toCharArray();
+        square[2] = "M VLN".toCharArray();
+        square[3] = "WPABE".toCharArray();
+        square[4] = "UQHCF".toCharArray();
+        printSquare(0, basic("ARRBBL0"));
+    }
+
+    @Test
+    public void test1() {
+        square[0] = "ABCDE".toCharArray();
+        square[1] = "FGHIJ".toCharArray();
+        square[2] = "KLMNO".toCharArray();
+        square[3] = "PQRS ".toCharArray();
+        square[4] = "TUVWX".toCharArray();
+        printSquare(1, basic("AAALLLL0"));
+    }
+
+    @Test
+    public void test2() {
+        square[0] = "ABCDE".toCharArray();
+        square[1] = "FGHIJ".toCharArray();
+        square[2] = "KLMNO".toCharArray();
+        square[3] = "PQRS ".toCharArray();
+        square[4] = "TUVWX".toCharArray();
+        printSquare(2, basic("AAAAABBRRRLL0"));
+
+    }
+
+    private void printSquare(int no, boolean pass) {
+        System.out.printf("Puzzle #%d:\n", no);
+        if (!pass) {
+            System.out.println("This puzzle has no final configuration.");
+            return;
+        }
+        for (int i = 0; i < SIZE; ++i) {
+            System.out.printf("%c %c %c %c %c\n",
+                    square[i][0], square[i][1], square[i][2], square[i][3], square[i][4]);
+        }
+    }
+    /**
+     *  时间复杂度：O(n)
+     *  空间复杂度：O(1)
+     */
+    private boolean basic(String seq) {
+        Point space = new Point();
+        boolean pass = true;
+        for (int i = 0, len = seq.length() - 1; i < len && pass; ++i) {
+            switch (seq.charAt(i)) {
+                case 'A':
+                    pass = space.moveAbove();
+                    break;
+                case 'B':
+                    pass = space.moveBelow();
+                    break;
+                case 'R':
+                    pass = space.moveRight();
+                    break;
+                case 'L':
+                    pass = space.moveLeft();
+                    break;
+                default:
+                    break;
+            }
+        }
+        return pass;
+    }
+
+    class Point {
+        private int x;
+        private int y;
+
+        public Point() {
+            for (int i = 0; i < SIZE; ++i) {
+                for (int j = 0; j < SIZE; ++j) {
+                    if (square[i][j] == ' ') {
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+
+        public boolean moveLeft() {
+            if (y == 0) {
+                return false;
+            }
+            square[x][y] = square[x][y - 1];
+            square[x][y - 1] = ' ';
+            y -= 1;
+            return true;
+        }
+
+        public boolean moveAbove() {
+            if (x == 0) {
+                return false;
+            }
+            square[x][y] = square[x - 1][y];
+            square[x - 1][y] = ' ';
+            x -= 1;
+            return true;
+        }
+
+        public boolean moveBelow() {
+            if (x == SIZE - 1) {
+                return false;
+            }
+            square[x][y] = square[x + 1][y];
+            square[x + 1][y] = ' ';
+            x += 1;
+            return true;
+        }
+
+        public boolean moveRight() {
+            if (y == SIZE - 1) {
+                return false;
+            }
+            square[x][y] = square[x][y + 1];
+            square[x][y + 1] = ' ';
+            y += 1;
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int num = 0;
+        while (true){
+            if(!processSquare(cin, ++num)){
+                break;
+            }
+        }
+    }
+
+    private static boolean processSquare(Scanner cin, int no) {
+        String firstLine = cin.nextLine();
+        if(firstLine.length() == 1 && firstLine.charAt(0) == 'Z'){
+            return false;
+        }
+
+        for(int i = 0; i < SIZE; ++i){
+            char[] chars;
+            if(i == 0){
+                chars = firstLine.toCharArray();
+            }else{
+                chars = cin.nextLine().toCharArray();
+            }
+            System.arraycopy(chars, 0, square[i], 0, chars.length);
+            if(chars.length != SIZE){
+                square[i][SIZE-1] = ' ';
+            }
+        }
+
+        StringBuilder builder = new StringBuilder(10);
+        while (true){
+            builder.append(cin.nextLine());
+            if(builder.charAt(builder.length() - 1) == '0'){
+                break;
+            }
+        }
+
+        Pra_3_5_Puzzle main = new Pra_3_5_Puzzle();
+        if(no != 1){
+            System.out.println();
+        }
+        main.printSquare(no, main.basic(builder.toString()));
+
+        return true;
+    }
+}
+
---
+++ src/main/java/me/rainstorm/chp4/Demo_4_1_AncientCipher.java
@@ -0,0 +1,74 @@
+package me.rainstorm.chp4;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 题目描述
+ *      https://uva.onlinejudge.org/external/13/1339.pdf
+ */
+public class Demo_4_1_AncientCipher {
+    @Test
+    public void test() {
+        basic("JWPUDJSTVP", "VICTORIOUS");
+    }
+
+    @Test
+    public void test1() {
+        basic("MAMA", "ROME");
+    }
+
+    @Test
+    public void test2() {
+        basic("HAHA", "HEHE");
+    }
+
+    @Test
+    public void test3() {
+        basic("AAA", "AAA");
+    }
+
+    @Test
+    public void test4() {
+        basic("NEERCISTHEBEST", "SECRETMESSAGES");
+    }
+
+
+    /**
+     * 时间复杂度：O(nlog(n))
+     * 空间复杂度：O(1)
+     */
+    public void basic(String str1, String str2) {
+        final int SIZE = 26;
+        int[] num1 = new int[SIZE];
+        int[] num2 = new int[SIZE];
+
+        for (int i = 0, len = str1.length(); i < len; ++i) {
+            num1[str1.charAt(i) - 'A']++;
+            num2[str2.charAt(i) - 'A']++;
+        }
+
+        Arrays.sort(num1);
+        Arrays.sort(num2);
+
+        for (int i = 0; i < SIZE; ++i) {
+            if (num1[i] != num2[i]) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+        return;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner cin = new Scanner(System.in);
+        Demo_4_1_AncientCipher main = new Demo_4_1_AncientCipher();
+        while (cin.hasNextLine()) {
+            main.basic(cin.nextLine(), cin.nextLine());
+        }
+    }
+}
